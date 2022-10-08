9be3e52e08d9b33830f27154d7ad3aabfcfee60e
ShanuDey <sd4shanudey@gmail.com>
2022-10-08 12:26:42 +0530

    using chrome for github action test execution
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

---
+++ config/ci/wdio.chrome.conf.js
@@ -0,0 +1,29 @@
+const { config } = require("../wdio.shared.conf");
+
+exports.config = {
+  ...config,
+  // Level of logging verbosity: trace | debug | info | warn | error | silent
+  logLevel: "error",
+  capabilities: [
+    {
+      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+      // grid with only 5 firefox instances available you can make sure that not more than
+      // 5 instances get started at a time.
+      maxInstances: 5,
+      //
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["-headless"],
+      },
+      acceptInsecureCerts: true,
+      // If outputDir is provided WebdriverIO can capture driver session logs
+      // it is possible to configure which logTypes to include/exclude.
+      // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+      // excludeDriverLogs: ['bugreport', 'server'],
+    },
+  ],
+
+  onComplete: function () {
+    console.log("Test Execution Completed");
+  },
+};
